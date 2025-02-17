c092927af985be8c4683598431fe76dd7aa02ae2
IIIMADDINIII <martin@elend.name>
2025-02-17 19:06:25 +0100

    v0.0.2

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -22,3 +22,22 @@ export const buildCi = tools.exitAfter(
 export const test = tools.exitAfter(
   tasks.installDependencies(),
   tasks.rollup.buildAndRunTests(buildOptions));
+
+export async function version() {
+  let arg = process.argv.at(-1);
+  if (arg === undefined || !arg.startsWith("--")) arg = "--patch";
+  arg = arg.slice(2);
+  await tools.exec`pnpm version -f --no-git-tag-version ${arg}`;
+  const version = await tools.getPackageVersion(undefined, false);
+  await tools.createCommit({ message: `v${version}` });
+}
+
+export async function publish() {
+  await tools.cleanWithGit();
+  await tools.prodInstallDependencies();
+  await tools.rollup.buildAndRunTests(buildOptions);
+  await tools.ensureGitIsClean();
+  await version();
+  await tools.exec`pnpm -r publish`;
+  await tools.exec`git push`;
+}
