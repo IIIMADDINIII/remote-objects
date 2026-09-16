8f7b4d629fccaaf78c84fb71c7409b14de633964
IIIMADDINIII <martin@elend.name>
2026-09-16 21:32:37 +0200

    Fixing problems with silent errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,44 @@
 import accessPrivates from "rolldown-plugin-access-privates";
 import { defineConfig } from "vite-plus";
 
-export default defineConfig(({ mode }) => ({
-  plugins: mode === "test" ? [accessPrivates()] : [],
-  pack: {
-    entry: ["./src/index.ts", "./src/RequestHandler.ts", "./src/ObjectStore.ts"],
-    dts: true,
-    exports: true,
-    format: ["esm", "cjs"],
-  },
-  test: {
-    coverage: {
-      enabled: true,
+export default defineConfig((env) => {
+  return {
+    plugins: env.mode === "test" ? [accessPrivates()] : [],
+    pack: {
+      entry: ["./src/index.ts", "./src/RequestHandler.ts", "./src/ObjectStore.ts"],
+      dts: true,
+      exports: true,
+      platform: "neutral",
+      format: ["esm", "cjs"],
+      failOnWarn: true,
     },
-    execArgv: ["--expose-gc"],
-    typecheck: {
-      enabled: true,
+    test: {
+      coverage: {
+        enabled: true,
+      },
+      execArgv: ["--expose-gc"],
+      typecheck: {
+        enabled: true,
+      },
     },
-  },
-  lint: {
-    ignorePatterns: ["/mise/"],
-    options: {
-      typeAware: true,
-      typeCheck: true,
+    lint: {
+      jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+      rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+      ignorePatterns: ["/**/mise/", "/**/declarations.d.ts"],
+      options: {
+        typeAware: true,
+        typeCheck: true,
+        denyWarnings: true,
+      },
     },
-  },
-  fmt: {
-    ignorePatterns: ["/mise/"],
-    sortImports: true,
-    printWidth: 300,
-    jsdoc: {
-      descriptionWithDot: true,
-      lineWrappingStyle: "balance",
+    fmt: {
+      ignorePatterns: ["/mise/"],
+      sortImports: true,
+      printWidth: 300,
+      jsdoc: {
+        descriptionWithDot: true,
+        lineWrappingStyle: "balance",
+      },
     },
-  },
-}));
+  };
+});
